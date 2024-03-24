420d5012e739797809ff3286d614e80f12b1b2a9
270516 <ranjankomal93@gmail.com>
2024-03-24 13:25:08 +0100

    some changes

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import { LandingPage } from './containers/LandingPage/LandingPage.Container';
 import { PageNotFound } from './containers/PageNotFound/PageNotFound.Container';
+import { IndexPage } from './Pages/IndexPage';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="*" element={<PageNotFound />} />
+          <Route path="/indexpage" element={<IndexPage />} />
         </Routes>
       </Router>
     </div>
---
+++ packages/client/src/Pages/IndexPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const IndexPage = () => {
+  return (
+    <div>
+      <h1>Welcome to my index page</h1>
+      <p>Here you can join the retro or create a team for new retro</p>
+    </div>
+  );
+};
