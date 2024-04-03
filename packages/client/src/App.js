b5a8fcc26a03835790a0efc19ee0346649ec12eb
Aaron Moses <pastures77@gmail.com>
2024-04-04 00:54:04 +0200

    Added RetroPage2 component with some CSS and  tested with App.js

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import { Navbar } from './containers/NavbarPage/Navbar';
+import RetroPage2 from './containers/RetroPageSecond/RetroPage2';
 import IndexPage from './containers/IndexPage/IndexPage';
 // import { LandingPage } from './containers/LandingPage/LandingPage.Container';
 import { PageNotFound } from './containers/PageNotFound/PageNotFound.Container';
@@ -13,6 +14,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
+          <Route path='/' element={<RetroPage2 />} />
           <Route path="/" element={<IndexPage />} />
           {/* <Route path="/" element={<LandingPage />} /> */}
           <Route path="/CreateNewTeamPage" element={<CreateTeam />} />
