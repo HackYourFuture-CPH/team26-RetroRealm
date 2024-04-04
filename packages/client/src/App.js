fea07fb46ee9fb258f958c62a82ca1cb10670d12
Aaron Moses <pastures77@gmail.com>
2024-04-04 11:01:34 +0200

    Fixed prettier error

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -14,7 +14,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' element={<RetroPage2 />} />
+          <Route path="/" element={<RetroPage2 />} />
           <Route path="/" element={<IndexPage />} />
           {/* <Route path="/" element={<LandingPage />} /> */}
           <Route path="/CreateNewTeamPage" element={<CreateTeam />} />
--- packages/client/src/containers/RetroPageSecond/RetroPage2.jsx
+++ packages/client/src/containers/RetroPageSecond/RetroPage2.jsx
@@ -6,7 +6,7 @@ const questions = [
   { id: 1, text: 'What did we do well?' },
   { id: 2, text: 'What did we learn?' },
   { id: 3, text: 'What should we do differently next time?' },
-  { id: 4, text: 'What are the roadblocks?' }
+  { id: 4, text: 'What are the roadblocks?' },
 ];
 
 function RetroPage2() {
@@ -28,12 +28,15 @@ function RetroPage2() {
     if (!commentText) return;
 
     const newCommentId = Math.random().toString(36).substring(2, 10);
-    setComments(prevComments => ({
+    setComments((prevComments) => ({
       ...prevComments,
-      [questionId]: [...(prevComments[questionId] || []), { id: newCommentId, text: commentText }]
+      [questionId]: [
+        ...(prevComments[questionId] || []),
+        { id: newCommentId, text: commentText },
+      ],
     }));
 
-    setInputValues(prevValues => ({ ...prevValues, [questionId]: '' }));
+    setInputValues((prevValues) => ({ ...prevValues, [questionId]: '' }));
   };
 
   const handleComplete = () => {
@@ -49,7 +52,9 @@ function RetroPage2() {
       const response = await fetch(`/api/retros/${joinCode}`);
 
       if (!response.ok) {
-        throw new Error(`Failed to fetch retro session: ${response.statusText}`);
+        throw new Error(
+          `Failed to fetch retro session: ${response.statusText}`,
+        );
       }
 
       const retroSession = await response.json();
@@ -59,8 +64,10 @@ function RetroPage2() {
       setComments(retroSession.comments);
       setJoinCode('');
     } catch (error) {
-       // eslint-disable-next-line no-alert
-      alert('Failed to join the retro session. Please check the code and try again.');
+      // eslint-disable-next-line no-alert
+      alert(
+        'Failed to join the retro session. Please check the code and try again.',
+      );
     }
   }, [joinCode]);
 
@@ -68,58 +75,81 @@ function RetroPage2() {
     if (e.key === 'Enter') {
       handleAddComment(questionId);
     } else {
-      setInputValues(prevValues => ({ ...prevValues, [questionId]: e.target.value }));
+      setInputValues((prevValues) => ({
+        ...prevValues,
+        [questionId]: e.target.value,
+      }));
     }
   };
 
   return (
     <div>
-      <div className='retroContainer'>
-        <div className='retroHeader'>
-      <h2>Retro</h2>
-      <button className="newRetro-button" type='button' onClick={handleNewRetro}>New Retro</button>
+      <div className="retroContainer">
+        <div className="retroHeader">
+          <h2>Retro</h2>
+          <button
+            className="newRetro-button"
+            type="button"
+            onClick={handleNewRetro}
+          >
+            New Retro
+          </button>
+        </div>
+        {retroCode && <p>Retro code: {retroCode}</p>}
+        <input
+          className="joinCodeInput"
+          type="text"
+          placeholder="Enter code to join"
+          value={joinCode}
+          onChange={(e) => setJoinCode(e.target.value)}
+        />
+        <button className="joinButton" type="button" onClick={handleJoin}>
+          Join
+        </button>
       </div>
-      {retroCode && <p>Retro code: {retroCode}</p>}
-      <input className='joinCodeInput'
-        type="text"
-        placeholder="Enter code to join"
-        value={joinCode}
-        onChange={(e) => setJoinCode(e.target.value)}
-      />
-      <button className='joinButton' type='button' onClick={handleJoin}>Join</button>
-    </div>
-     {selectedQuestions.map(question => (
+      {selectedQuestions.map((question) => (
         <div key={question.id} className="container">
-          <div className="question">{question.text}
-          <div className="box">
-            {comments[question.id] && comments[question.id].map(comment => (
-              <div key={comment.id} className="comment">{comment.text}</div>
-            ))}
-            <select className="roleSelect">
+          <div className="question">
+            {question.text}
+            <div className="box">
+              {comments[question.id] &&
+                comments[question.id].map((comment) => (
+                  <div key={comment.id} className="comment">
+                    {comment.text}
+                  </div>
+                ))}
+              <select className="roleSelect">
                 <option value="">Select role</option>
                 <option value="teamMember">Team Member</option>
                 <option value="teamLeader">Team Leader</option>
               </select>
 
-            <input
-              type="text"
-              placeholder="Add a comment"
-              value={inputValues[question.id] || ''}
-              onChange={(e) => handleCommentInput(question.id, e)}
-              onKeyDown={(e) => e.key === 'Enter' && handleCommentInput(question.id, e)}
-              className="commentInput"
-            />
-          
-            <button type="button" onClick={() => handleAddComment(question.id)} className="addCommentButton">
-              +
-            </button>
+              <input
+                type="text"
+                placeholder="Add a comment"
+                value={inputValues[question.id] || ''}
+                onChange={(e) => handleCommentInput(question.id, e)}
+                onKeyDown={(e) =>
+                  e.key === 'Enter' && handleCommentInput(question.id, e)
+                }
+                className="commentInput"
+              />
+
+              <button
+                type="button"
+                onClick={() => handleAddComment(question.id)}
+                className="addCommentButton"
+              >
+                +
+              </button>
+            </div>
           </div>
-         </div>
         </div>
       ))}
-    
 
-      <button className='completeButton' type='button' onClick={handleComplete}>Complete</button>
+      <button className="completeButton" type="button" onClick={handleComplete}>
+        Complete
+      </button>
     </div>
   );
 }
