d5357c4a5a8f2c1bb255792b24adcfbd7dec686b
Nishadi <nish.shanika@gmail.com>
2024-04-14 07:48:40 +0200

    modified handle submit function

--- packages/client/src/containers/IndexPage/IndexPage.jsx
+++ packages/client/src/containers/IndexPage/IndexPage.jsx
@@ -18,12 +18,19 @@ function IndexPage() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (inputValue === 'TeamCode') {
+    if (inputValue.trim() !== '') {
       try {
-        const response = await fetch(`${apiURL}/CreateNewTeam/${inputValue}`);
+        const response = await fetch(`${apiURL}/validateTeamCode`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ teamCode: inputValue }),
+        });
         if (response.ok) {
           // Navigate to retroPage
-          navigate('/JoinRetroPage');
+
+          navigate('/retroPage');
         } else {
           setErrorMessage(
             'Invalid team code. Please try again or make sure you entered the correct team code.',
