fe280c6c56c996790b0eaef03f959cec0526af52
Nishadi <nish.shanika@gmail.com>
2024-04-18 11:39:18 +0200

    fixed prttier errors

--- packages/server/seeds/development/questions.js
+++ packages/server/seeds/development/questions.js
@@ -1,14 +1,14 @@
-exports.seed = function(knex) {
-    // Deletes all existing entries
-    return knex('Questions').del()
-      .then(function () {
-        // Inserts seed entries
-        return knex('Questions').insert([
-          { id: 1, title: 'What went well?', color: 'green' },
-          { id: 2, title: 'What didn\'t go well?', color: 'red' },
-          { id: 3, title: 'What can be improved?', color: 'yellow' },
-          { id: 4, title: 'Any other comments?', color: 'blue' }
-        ]);
-      });
-  };
-  
+exports.seed = function (knex) {
+  // Deletes all existing entries
+  return knex('Questions')
+    .del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('Questions').insert([
+        { id: 1, title: 'What went well?', color: 'green' },
+        { id: 2, title: "What didn't go well?", color: 'red' },
+        { id: 3, title: 'What can be improved?', color: 'yellow' },
+        { id: 4, title: 'Any other comments?', color: 'blue' },
+      ]);
+    });
+};
