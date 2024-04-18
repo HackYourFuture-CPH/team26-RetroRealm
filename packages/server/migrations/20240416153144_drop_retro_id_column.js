79b6879c7192f6b957bffcb9b22715925d21d5d4
Nishadi <nish.shanika@gmail.com>
2024-04-18 11:49:28 +0200

    fixed prttier errors

--- packages/server/migrations/20240416153144_drop_retro_id_column.js
+++ packages/server/migrations/20240416153144_drop_retro_id_column.js
@@ -3,24 +3,25 @@
  * @returns { Promise<void> }
  */
 exports.up = async (knex) => {
-    await knex.raw('ALTER TABLE `Questions` DROP CONSTRAINT questions_retro_id_foreign');
-    await knex.schema.alterTable('Questions', (table) => {
-        table.dropColumn('retro_id');
-    });
-  };
-  
-  /**
-   * @param { import("knex").Knex } knex
-   * @returns { Promise<void> }
-   */
-  exports.down = async (knex) => {
-    await knex.schema.alterTable('Answers', (table) => {
-        table
-        .integer('retro_id')
-        .unsigned()
-        .notNullable()
-        .references('id')
-        .inTable('Retro');
-    });
-  };
-  
+  await knex.raw(
+    'ALTER TABLE `Questions` DROP CONSTRAINT questions_retro_id_foreign',
+  );
+  await knex.schema.alterTable('Questions', (table) => {
+    table.dropColumn('retro_id');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async (knex) => {
+  await knex.schema.alterTable('Answers', (table) => {
+    table
+      .integer('retro_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('Retro');
+  });
+};
