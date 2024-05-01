786c2e082f197d17046ea4d3f8f7b73786efd38b
Aaron Moses <pastures77@gmail.com>
2024-05-01 09:17:25 +0200

    failed migration build so added questions.js seed file to add color property

--- packages/server/seeds/development/questions.js
+++ packages/server/seeds/development/questions.js
@@ -3,11 +3,19 @@
  * @returns { Promise<void> }
  */
 // questions.js
-exports.seed = function (knex) {
-  return knex('Questions').insert([
-    { id: 1, title: 'What did we do well?' },
-    { id: 2, title: 'What did we learn?' },
-    { id: 3, title: 'What should we do differently next time?' },
-    { id: 4, title: 'What are the roadblocks?' },
+module.exports.seed = async function (knex) {
+  // Deletes ALL existing entries
+  await knex('Questions').del();
+
+  // Inserts seed entries
+  await knex('Questions').insert([
+    { id: 1, title: 'What did we do well?', color: 'red' },
+    { id: 2, title: 'What did we learn?', color: 'blue' },
+    {
+      id: 3,
+      title: 'What should we do differently next time?',
+      color: 'green',
+    },
+    { id: 4, title: 'What are the roadblocks?', color: 'yellow' },
   ]);
 };
