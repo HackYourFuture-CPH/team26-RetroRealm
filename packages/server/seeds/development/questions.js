950c7bc1cb373a3ed4642e2137e350b7b587584e
Aaron Moses <pastures77@gmail.com>
2024-05-01 08:36:56 +0200

    fixed migration questions.js seed file

--- packages/server/seeds/development/questions.js
+++ packages/server/seeds/development/questions.js
@@ -1,14 +1,13 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+// questions.js
 exports.seed = function (knex) {
-  // Deletes all existing entries
-  return knex('Questions')
-    .del()
-    .then(() => {
-      // Inserts seed entries
-      return knex('Questions').insert([
-        { id: 1, title: 'What did we do well?' },
-        { id: 2, title: 'What did we learn?' },
-        { id: 3, title: 'What should we do differently next time?' },
-        { id: 4, title: 'What are the roadblocks?' },
-      ]);
-    });
+  return knex('Questions').insert([
+    { id: 1, title: 'What did we do well?' },
+    { id: 2, title: 'What did we learn?' },
+    { id: 3, title: 'What should we do differently next time?' },
+    { id: 4, title: 'What are the roadblocks?' },
+  ]);
 };
